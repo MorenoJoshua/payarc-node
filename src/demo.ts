6874c9c82a495bfb0f734ecd0ec84c7307a44d68
Joshua Moreno <joshua200128@gmail.com>
2020-03-20 17:26:26 -0700

    Demo Data

---
+++ src/demo.ts
@@ -0,0 +1,6 @@
+export const demoCards = [{company: "Visa", number: "4012000098765439", month: 12, year: 2020, cvv: 999},
+  {company: "Mastercard", number: "5146315000000055", month: 12, year: 2020, cvv: 998},
+  {company: "Amex", number: "371449635392376", month: 12, year: 2020, cvv: 9997},
+  {company: "Discover", number: "6011000993026909", month: 12, year: 2020, cvv: 996},
+  {company: "Diners", number: "3055155515160018", month: 12, year: 2020, cvv: 996},
+  {company: "JCB", number: "3530142019945859", month: 12, year: 2020, cvv: 996},];
